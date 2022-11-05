20ceb38b6389c653aae380b8c6113e5ea53aafe9
Tiffany <hey@tiffanyrwhite.com>
2022-11-05 01:43:44 -0400

    Edit

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,97 +1,118 @@
 export const info = {
-  baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
-  jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  baseUrl: "https://testing.tiffanyrwhite.com",
+  name: "Tiffany White",
+  jobDescription: "Frontend Software Engineer",
+  about: `I'm a software engineer based in Pennsylvania.
+   I like to write about frontend development, tinker with electronics, and muse about 
+   software engineering topics on my Obsidian Publish site to #LearnInPublic.
+   
+   
+   Note: The code of two of the projects below are proprietary, and as such, don't have a public GitHub link. 
+   Sorry about that!
+
   `,
 
-  experience: [
-    {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
-      endDate: "Present",
-      description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
-      ],
-    },
-
-    {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
-      description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
-      ],
-    },
-
-    {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
-      description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
-      ],
-    },
-  ],
-
-  education: [
-    {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
-      description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
-      ],
-    },
-    {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
-    },
-    {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
-    },
-  ],
+  // experience: [
+  //   {
+  //     name: "InterNative Labs, LLC",
+  //     location: "Karāchi, Sindh, Pakistan",
+  //     startDate: "Apr 2022",
+  //     endDate: "Present",
+  //     description: [
+  //       "- Developing screens and UI components for the web application using React and Tailwind.",
+  //       "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+  //     ],
+  //   },
+  //
+  //   {
+  //     name: "Coupon Approve (Remote)",
+  //     location: "Amsterdam, North Holland, Netherlands",
+  //     startDate: "Nov 2020",
+  //     endDate: "Apr 2022",
+  //     description: [
+  //       "✓ Developing code in accordance with appropriate design and architectural rules",
+  //       "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
+  //       "✓ Help and training of other colleagues",
+  //       "✓ Others tasks connected to the nature of the job",
+  //       "✓ Code Improvements and Refactoring ",
+  //       "✓ Best Practices and Rules Maintainer",
+  //     ],
+  //   },
+  //
+  //   {
+  //     name: "Interns Pakistan",
+  //     location: "Pakistan",
+  //     startDate: "Oct 2020",
+  //     endDate: "Nov 2020",
+  //     description: [
+  //       "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
+  //       "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+  //     ],
+  //   },
+  // ],
+  //
+  // education: [
+  //   {
+  //     name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
+  //     location: "Karāchi, Sindh, Pakistan",
+  //     startDate: "2019",
+  //     endDate: "2022",
+  //     description: [
+  //       "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+  //     ],
+  //   },
+  //   {
+  //     name: "Govt. Degree Boys College Buffer Zone",
+  //     location: "Karāchi, Sindh, Pakistan",
+  //     startDate: "2018",
+  //     endDate: "2020",
+  //     description: ["Intermediate, Pre-Engineering"],
+  //   },
+  //   {
+  //     name: "Ghazi Foundation Schools & Colleges",
+  //     location: "Karāchi, Sindh, Pakistan",
+  //     startDate: "2016",
+  //     endDate: "2018",
+  //     description: ["Matriculation,  Computer Science"],
+  //   },
+  // ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    notes: "https://tiffnotes.dev",
+    blog: "https://tiffanywhite.dev",
+    github: "https://github.com/twhite96",
+    email: "tiffany@tiffanyrwhite.com",
+    linkedin: "https://www.linkedin.com/in/tiffanywhite/",
   },
 
   projects: [
     {
-      title: "Astro Portfolio",
+      title: "Calaxy, Inc. Landing Page",
       isFeatured: true,
-      thumbnail: "/assets/images/astro-portfolio.png",
-      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
-      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+      thumbnail: "https://res.cloudinary.com/twhiteblog/image/upload/v1667543020/calaxy-webflow_ixujbx.webp",
+      githubUrl: "",
+      liveUrl: "https://calaxy.com",
+    },
+    {
+      title: "Bumped, Inc. Landing Page",
+      isFeatured: true,
+      thumbnail: "https://res.cloudinary.com/twhiteblog/image/upload/v1667543019/bumped-v1_shoafo.webp",
+      githubUrl: "",
+      liveUrl: "https://bumped.com",
+    },
+    {
+      title: "v5 of my Gatsby Portfolio",
+      isFeatured: true,
+      thumbnail: "https://res.cloudinary.com/twhiteblog/image/upload/c_scale,w_2525/v1667543018/old-portfolio_tyg5vx.webp",
+      githubUrl: "https://github.com/twhite96/portfolio-v5",
+      liveUrl: "https://twhite-portfolio.netlify.app",
+    },
+    {
+      title: "Static Site Generator, Written in Ruby",
+      isFeatured: true,
+      thumbnail: "https://res.cloudinary.com/twhiteblog/image/upload/c_scale,w_1946/v1667626387/ruby-2_gn6h07.webp",
+      githubUrl: "https://github.com/twhite96/static-gen-ruby",
+      liveUrl: "https://rubygen.tiff.run/",
     },
   ],
 };
